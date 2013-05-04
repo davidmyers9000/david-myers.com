915c35e8c3f411bbfcd61d17321ee2977ed47697
David Myers <davidmyers9000@gmail.com>
2013-05-04 13:43:46 -0400

    Initial

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>La Casa de David Myers</title>
+		<!-- Bootswatch Cosmo Theme "http://bootswatch.com/cosmo/" -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css" rel="stylesheet">	
+		<!-- Bootstrap, jQuery -->
+		<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" ></script>
+		<!-- Respond.js https://github.com/scottjehl/Respond-->
+</head>
+<body>
+	<!--nav-->
+	
+	<!--feature-->
+	<!---->
+</body>
+</html>

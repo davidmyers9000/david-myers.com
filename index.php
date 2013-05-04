df816c8bf9caf5a5cd523a9ac8206d8767eeeac2
David Myers <davidmyers9000@gmail.com>
2013-05-04 14:35:35 -0400

    nav.php

--- index.php
+++ index.php
@@ -9,28 +9,10 @@
 		<!-- Respond.js https://github.com/scottjehl/Respond-->
 </head>
 <body>
-	<!--nav-->
-<div class="navbar navbar-fixed-top">
-	<div class="navbar-inner">
-     <div class="container">
-       <a data-target=".nav-collapse" data-toggle="collapse" class="btn btn-navbar">
-         <span class="icon-bar"></span>
-         <span class="icon-bar"></span>
-         <span class="icon-bar"></span>
-       </a>
-       <a href="../" class="brand">David Myers</a>
-       <div id="main-menu" class="nav-collapse collapse">
-        
-        <ul id="main-menu-right" class="nav pull-right">
-          <li><a title="" href="../" rel="tooltip">Portfolio<i class="icon-share-alt"></i></a></li>
-          <li><a title="" href="resume/" target="_blank" rel="tooltip">Resume<i class="icon-share-alt"></i></a></li>
-        </ul>
-       </div>
-     </div>
-   </div>
-</div>
-	<!--feature-->
-	<!---->
+	<?PHP include 'nav.php' ?>
+	<!-- feature -->
+	
+	<!-- footer -->
 	
 	<!-- jQuery, then Bootstrap -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" ></script>
---
+++ nav.php
@@ -0,0 +1,20 @@
+<!--nav-->
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+	     <div class="container">
+	       <a data-target=".nav-collapse" data-toggle="collapse" class="btn btn-navbar">
+	         <span class="icon-bar"></span>
+	         <span class="icon-bar"></span>
+	         <span class="icon-bar"></span>
+	       </a>
+	       <a href="../" class="brand">David Myers</a>
+	       <div id="main-menu" class="nav-collapse collapse">
+	        
+	        <ul id="main-menu-right" class="nav pull-right">
+	          <li><a title="" href="../" rel="tooltip">Portfolio<i class="icon-share-alt"></i></a></li>
+	          <li><a title="" href="resume/" target="_blank" rel="tooltip">Resume<i class="icon-share-alt"></i></a></li>
+	        </ul>
+	       </div>
+	     </div>
+	   </div>
+	</div>

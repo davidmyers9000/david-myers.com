50c09a461303f23b84914cf31e4125a6215e5a7f
David Myers <davidmyers9000@gmail.com>
2013-05-04 19:01:59 -0400

    Modal added (but broken)

--- index.php
+++ index.php
@@ -18,47 +18,79 @@
 	  <div class="page-header">
 	    <h1>Portfolio</h1>
 	  </div>
-	
-	  <!-- Headings & Paragraph Copy -->
+<!--=== portfolio item ===-->	
 	  <div class="row">
-	
 	    <div class="span12">
 	      <div class="well">
-	        <h1>Image</h1>
+	        <h1>Heart of the Island</h1>
 	      </div>
 	    </div>
 	  </div>
+	  
+	  <div class="row">
+	    <div class="span12">
+	        <img src="http://david-myers.com/img/thumbnails/hoti-thumb-large.jpg" />
+	      </div>
+	    </div>
+	  </div>
+	  
 	  <div class="row">	
-	    <div class="span6">
-	      <h2>Project Title <small>My role</small></h2>
-	      <p>Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-	      <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui.</p>
+	    <div class="span8">
+	      <h2>Adventure Game for iPad</h2>
+	      <h4>Mobile Game Design/Development</h4>
+	      <p>Heart of the Island is a first person adventure game for the iPad. The player wakes up on a mysterious island. The player explores, solve puzzles, and makes decisions along the way that have an outcome on the final results. The game innovates upon the mystery/puzzle solving genre by using a "real-time" navigation system by using virtual joy sticks.</p>
 	    </div>
 	
-	    <div class="span6">
+	    <div class="span4">
 	      <h3>Links</h3>
 	      <address>
-	        <strong>Live website</strong><br>
-	        <a href="mailto:#">first.last@gmail.com</a>
+	        <strong>Dev Blog</strong><br>
+	        <a href="http://teamidentitycrisis.wordpress.com/">Blog</a>
 	      </address>
-	      <span class="badge">Indie</span>
-	      <span class="badge badge-success">Games</span>
-	      <span class="badge badge-warning">Animation</span>
-	      <span class="badge badge-important">UI</span>
-	      <span class="badge badge-info">Research</span>
-	      <span class="badge badge-inverse">3D</span>
+	      <address>
+	        <strong>Videos</strong><br>
+	        <a href="hoti-trailer.html" role="button" class="btn" data-remote="hoti-trailer.html" data-toggle="modal">Trailer</a>
+	      </address>
+	      <span class="label label-success">Game</span>
+	      <span class="label label-inverse">3D</span>
+	      
 	    </div>
 	
-	  </div>
+	  </div><!--=== End item ===-->
 	  	
 	</section>
 
 </div>
+
+<!-- Modal -->
+<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    <h3 id="myModalLabel">Modal header</h3>
+  </div>
+  <div class="modal-body">
+  </div>
+  <div class="modal-footer">
+    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+    <button class="btn btn-primary">Save changes</button>
+  </div>
+</div>
+
+<!--
+	      <span class="label">Indie</span>
+	      <span class="label label-success">Game</span>
+	      <span class="label label-warning">Animation</span>
+	      <span class="label label-important">UI</span>
+	      <span class="label label-info">Research</span>
+	      <span class="label label-inverse">3D</span>
+	      -->
 	<!-- footer -->
 	
 	<!-- jQuery, then Bootstrap -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" ></script>
 	<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.js"></script>
+	<!-- Modal Remote Fix -->
+	
 
 </body>
 </html>

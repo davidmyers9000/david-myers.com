e20b8f75beae3174881a7f2e7f69f70dd77a09e0
David Myers <davidmyers9000@gmail.com>
2013-05-05 14:18:06 -0400

    git issues

--- index.php
+++ index.php
@@ -6,30 +6,258 @@
 		<!-- Bootswatch Cosmo Theme "http://bootswatch.com/cosmo/" -->
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css" rel="stylesheet">	
+		<link href="css/main.css" rel="stylesheet" />
 		<!-- Respond.js https://github.com/scottjehl/Respond-->
 </head>
 <body>
-	<?PHP include 'nav.php' ?>
+	<!--<?PHP include 'nav.php' ?>-->
+	<!--nav-->
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+	     <div class="container">
+	       <a data-target=".nav-collapse" data-toggle="collapse" class="btn btn-navbar">
+	         <span class="icon-bar"></span>
+	         <span class="icon-bar"></span>
+	         <span class="icon-bar"></span>
+	       </a>
+	       <a href="../" class="brand">David Myers</a>
+	       <div id="main-menu" class="nav-collapse collapse">
+	        
+	        <ul id="main-menu-right" class="nav pull-right">
+	          <li><a title="" href="../" rel="tooltip">Portfolio<i class="icon-share-alt"></i></a></li>
+	          <li><a title="" href="resume/" target="_blank" rel="tooltip">Resume<i class="icon-share-alt"></i></a></li>
+	          <li class="dropdown" id="preview-menu">
+	            <a class="dropdown-toggle" data-toggle="dropdown" href="#">Contact <b class="caret"></b></a>
+	            <ul class="dropdown-menu">
+	              <li><a target="_blank" href="mailto:davidmyers9000@gmail.com">Gmail</a></li>
+	              <li class="divider"></li>
+	              <li><a target="_blank" href="//www.linkedin.com/pub/david-myers/23/b59/8/">LinkedIn</a></li>
+	              <li><a target="_blank" href="//www.twitter.com/_Dmyers">Twitter</a></li>
+	              <li><a target="_blank" href="//www.facebook.com/DaveyMShotBladeakaMCFury">Facebook</a></li>
+	            </ul>
+          </li>
+	        </ul>
+	       </div>
+	     </div>
+	   </div>
+	</div>
 
 <!-- feature -->
-<div id="container">	
-	<section id="typography">
-		
-	  <div class="page-header">
-	    <h1>Portfolio</h1>
-	  </div>
-<!--=== portfolio item ===-->	
+<div id="container">
+	<section id="portfolio">
+	
+<!--=== portfolio item 1 ===-->	
+	<article>
 	  <div class="row">
 	    <div class="span12">
-	      <div class="well">
-	        <h1>Heart of the Island</h1>
-	      </div>
+	        <img src="//c767204.r4.cf2.rackcdn.com/d985a90f-af74-4958-a8c0-1a206cd6ab1f.jpg" />
 	    </div>
 	  </div>
 	  
 	  <div class="row">
 	    <div class="span12">
-	        <img src="http://david-myers.com/img/thumbnails/hoti-thumb-large.jpg" />
+	      <div class="well">
+	        <h1>Fitly</h1>
+	      </div>
+	    </div>
+	  </div>
+	  
+	  <div class="row">	
+	    <div class="span8">
+	      <h2>Startup</h2>
+	      <h4>Co-founder, Lead UI/UX Designer</h4>
+	      <p>Fitly is <em>one of just 10</em> startups accepted into the inaugural <a href="http://www.dreamitventures.com/programs/dreamithealth/about-dreamit-health/">Dreamit Health</a> class. Fitly addresses the childhood obesity epidemic at its core by providing families with free and convenient delivery of healthy meal plans, ingredients, and preparation instructions to the workplace.
+</p>
+	    </div>
+	
+	    <div class="span4">
+	      <h3>Links</h3>
+	      <address>
+	        <a href="http://fitly.org" target="_blank" role="button" class="btn btn-large btn-block btn-success">See it live</a>
+	      </address>
+	      <strong>Holler at us!</strong><br />
+	      <a target="_blank" href="//www.facebook.com/pages/Fitly-Making-eating-healthy-fun-for-kids/254561257918450" alt="Share on Facebook"><img src="img/share-facebook.png" alt="Like us on Facebook" /></a> 
+		  <a target="_blank" href="//twitter.com/fitlyfamily" data-via="FitlyFamily" data-size="large"><img src="img/share-twitter.png" alt="Tweet us on Twitter" /></a>
+	      <br>
+	      <strong>Tags</strong><br />
+	      <span class="badge badge-important">UI</span>
+
+	    </div>
+	
+	  </div><!--=== End item 1 ===-->
+	</article>
+<!--=== portfolio item 2 ===-->	
+	<article>
+	  <div class="row">
+	    <div class="span12">
+	        <img style="height:auto; width:100%" src="img/pco-banner.jpg" />
+	    </div>
+	  </div>
+	  
+	  <div class="row">
+	    <div class="span12">
+	      <div class="well">
+	        <h1>Drexel Biomedical Engineering</h1>
+	      </div>
+	    </div>
+	  </div>
+	  
+	  <div class="row">	
+	    <div class="span8">
+	      <h2>Philadelphia Computational Orchestra</h2>
+	      <h4>Web Designer</h4>
+	      <p><em>Currently Collaborating to make the website available in Turkish, as well as other languages.</em></p>
+	      <p>Philadelphia Computational Orchestra provides bioinformatics services in gene network construction, in silica determination of whole genome methylation barcodes, analysis of CHIP-Seq data, mutual information computations involving multiple proteins, and artificial intelligence methods used in predicting drug sensitivity.</p>
+	    </div>
+	
+	    <div class="span4">
+	      <h3>Links</h3>
+	      <address>
+	        <a href="http://pcobio.com" target="_blank" role="button" class="btn btn-large btn-block btn-success">See it live</a>
+	      </address>
+	      <strong>Tags</strong><br />
+	      <span class="badge badge-important">Web Design</span>
+
+	    </div>
+	
+	  </div>
+	</article>
+	  <!--=== End item 2 ===-->
+	  
+	  <!--=== portfolio item 3 ===-->	
+<!--=== portfolio item 4 ===-->	
+	<article>
+	  <div class="row">
+	    <div class="span12">
+	        <img style="height:auto; width:100%" src="img/dh.jpg" />
+	    </div>
+	  </div>
+	  
+	  <div class="row">
+	    <div class="span12">
+	      <div class="well">
+	        <h1>Digitas Health</h1>
+	      </div>
+	    </div>
+	  </div>
+	  
+	  <div class="row">	
+	    <div class="span8">
+	      <h2>Internship</h2>
+	      <h4>User Interaction Designer</h4>
+	      <p>Developed interactive Flash banner ads for 'Top 10' pharmaceutical companies. Designed both digital media and print materials e.g. websites, posters, emails, calendars, packaging, and storyboards</p>
+	    </div>
+	
+	    <div class="span4">
+	      <h3>Links</h3>
+	      <p><em>Due to an NDA signed, I may show only this work.</em></p>
+	      <address>
+	        <a href="//vimeo.com/46840182" target="_blank" role="button" class="btn btn-primary"><i class="icon-play icon-white"></i> "Health is Social"</a>
+	      </address>
+	      <strong>Tags</strong><br />
+	      <span class="badge badge-important">UI</span>
+	      <span class="badge badge-warning">Animation</span>
+
+	    </div>
+	  </article><!--=== End item 3 ===-->
+	  
+<!--=== portfolio item 4 ===-->	
+	<article>
+	  <div class="row">
+	    <div class="span12">
+	        <img style="height:auto; width:100%" src="img/star.jpg" />
+	    </div>
+	  </div>
+	  
+	  <div class="row">
+	    <div class="span12">
+	      <div class="well">
+	        <h1>Drexel S.T.A.R.</h1>
+	      </div>
+	    </div>
+	  </div>
+	  
+	  <div class="row">	
+	    <div class="span8">
+	      <h2>Digitally Preserving Ancient Artifacts</h2>
+	      <h4>Research Assistant</h4>
+	      <p>I scanned colonial artifacts at the Independence Living History Center. My work was some of the first of its kind to revolutionize a slow, outdated process of cataloguing ancient artifacts, as well as opened up an opportunity to create virtual interactive exhibits with our 3D scans.
+	      </p>
+	      <p>
+	      	The core of my work revolved around mastering a non-contact 3D laser scanner to scan and digitally preserve ancient artifacts. As seen above, I scanned a fossilized skeleton of the 65 Million-year-old Thoracasaurus. The scans were then used to reconstruct the extinct crocodylian in 3D by Drexel graduate student Evan Boucher. A "Paleo-art" project which won the National Geographic Lanzendorf PaleoArt Prize for Digital Modeling and Animation from the Society of Vertebrate Paleontology.
+	      </p>
+	    </div>
+	
+	    <div class="span4">
+	      <h3>Links</h3>
+	      <address>
+	        <a href="https://vimeo.com/14750657" target="_blank" role="button" class="btn btn-primary"><i class="icon-play icon-white"></i> Evan Boucher's Paleoart</a><br>
+	        <a href="http://www.ushistory.org/tour/living-history-center.htm" target="_blank" role="button" class="btn"><i class="icon-globe"></i> History Center</a><br>
+	        <a href="http://vertpaleo.org/Awards/Award-(15)" target="_blank" role="button" class="btn"><i class="icon-globe"></i> Lazendorf Prize</a><br>
+	        <a href="http://www.nsf.gov/" target="_blank" role="button" class="btn"><i class="icon-globe"></i> NSF</a><br>
+	      </address>
+	      <strong>Tags</strong><br />
+	      <span class="badge badge-info">Research</span>
+	      <span class="badge badge-inverse">3D</span>
+	    </div>
+	
+	  </div>
+	</article><!--=== End item 4 ===-->
+	  
+	  <!--=== portfolio item 5 ===-->	
+	<article>
+	  <div class="row">
+	    <div class="span12">
+	        <img style="height:auto; width:100%" src="img/up-banner.jpg" />
+	      </div>
+	  	</div>
+	  
+	  <div class="row">
+	    <div class="span12">
+	      <div class="well">
+	        <h1>Urban Playground</h1>
+	      </div>
+	    </div>
+	  </div>
+	  
+	  <div class="row">	
+	    <div class="span8">
+	      <h2>Fun, Social, Flashmobs</h2>
+	      <h4>Designer</h4>
+	      <p>Urban Playground is a student-run group that organizes fun improvisational events...better-known as "flash mobs". We have organized such events as massive freezes, a cross-city super-hero runs, international pillow fights, pants-less subway rides, MP3 Experiments, and much more.</p>
+	    </div>
+	
+	    <div class="span4">
+	      <h3>Videos</h3>
+	      <address>
+	        <a href="http://www.youtube.com/watch?v=cicutSJU5OQ" target="_blank" role="button" class="btn btn-primary"><i class="icon-play icon-white"></i> 30th St. Freeze</a><br>
+	        <a href="http://www.youtube.com/watch?v=zzudiutenYI" target="_blank" role="button" class="btn btn-primary"><i class="icon-play icon-white"></i>Internation Pillow Fight Day</a><br>
+	        <a href="http://youtu.be/1cmqkSWt2gE" target="_blank" role="button" class="btn btn-primary"><i class="icon-play icon-white"></i> Pant-less Subway Ride</a>
+	      </address>
+	      <strong>Holler at us!</strong><br />
+	      <a target="_blank" href="//www.facebook.com/groups/286801092417/?fref=ts" alt="Share on Facebook"><img src="img/share-facebook.png" alt="Like us on Facebook" /></a> 
+		  <a target="_blank" href="//twitter.com/urbnplayground" data-via="FitlyFamily" data-size="large"><img src="img/share-twitter.png" alt="Tweet us on Twitter" /></a>
+	      <br>
+	      <strong>Tags</strong><br />
+	      <span class="badge badge-important">Graphic Design</span>
+		  <span class="badge badge-success">Real-life Games</span>
+	    </div>
+	
+	  </div>
+	</article><!--=== End item 5 ===-->
+	  	  
+<!--=== portfolio item 6 ===-->	
+	<article>
+	  <div class="row">
+	    <div class="span12">
+	        <img style="height:auto; width:100%" src="http://david-myers.com/img/thumbnails/hoti-thumb-large.jpg" />
+	      </div>
+	    </div>
+	  
+	  <div class="row">
+	    <div class="span12">
+	      <div class="well">
+	        <h1>Heart of the Island</h1>
 	      </div>
 	    </div>
 	  </div>
@@ -44,29 +272,35 @@
 	    <div class="span4">
 	      <h3>Links</h3>
 	      <address>
+<<<<<<< HEAD
+	        <a href="#myModal" role="button" class="btn btn-large btn-block btn-primary" data-remote="hoti-trailer.html" data-toggle="modal"><i class="icon-play icon-white icon-white icon-white"></i> Trailer</a>
+=======
 	        <strong>Dev Blog</strong><br>
 	        <a href="#myModal" role="button" class="btn" data-remote="hoti-poly.html" data-toggle="modal">Blog</a>
 	      </address>
 	      <address>
 	        <strong>Videos</strong><br>
 	        <a href="#myModal" role="button" class="btn" data-remote="hoti-trailer.html" data-toggle="modal">Trailer</a>
+>>>>>>> ce67f3988439be942553953a4eaaf3bd70be156d
 	      </address>
-	      <span class="label label-success">Game</span>
-	      <span class="label label-inverse">3D</span>
+	      <strong>Tags</strong><br />
+	      <span class="badge badge-success">Game</span>
+	      <span class="badge badge-inverse">3D</span>
 	      
 	    </div>
 	
-	  </div><!--=== End item ===-->
+	  </div>
+	</article><!--=== End item 6 ===-->
 	  	
-	</section>
+</section><!-- End section -->
 
-</div>
+</div> <!-- End Container -->
 
 <!-- Modal -->
-<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-badgeledby="myModalbadge" aria-hidden="true">
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-    <h3 id="myModalLabel">Modal header</h3>
+    <h3 id="myModalbadge">Modal header</h3>
   </div>
   <div class="modal-body">
   	Loading Content...
@@ -81,23 +315,31 @@
 </div>
 
 <!--
-	      <span class="label">Indie</span>
-	      <span class="label label-success">Game</span>
-	      <span class="label label-warning">Animation</span>
-	      <span class="label label-important">UI</span>
-	      <span class="label label-info">Research</span>
-	      <span class="label label-inverse">3D</span>
+	      <span class="badge">Indie</span>
+	      <span class="badge badge-success">Game</span>
+	      <span class="badge badge-warning">Animation</span>
+	      <span class="badge badge-important">UI</span>
+	      <span class="badge badge-info">Research</span>
+	      <span class="badge badge-inverse">3D</span>
 	      -->
 	<!-- footer -->
 	
 	<!-- jQuery, then Bootstrap -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" ></script>
 	<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.js"></script>
+	<script src="//a.vimeocdn.com/js/froogaloop2.min.js?e46a8-1367711881"></script>
 	<!-- Modal Remote Fix -->
 	<script>
 		$('body').on('hidden', '.modal', function () {
+<<<<<<< HEAD
+		//refresh modal content
+			$(this).removeData('modal');
+		  
+		  });
+=======
 		  $(this).removeData('modal');
 		});
+>>>>>>> ce67f3988439be942553953a4eaaf3bd70be156d
 	</script>
 
 </body>
